ea94688b9e4520da6c1f7f3369036a980afb126b
AlessioAbbati <alex_117@live.it>
2023-04-14 16:33:50 +0200

    add fizz buzz fizzbuzz

--- js/myscript.js
+++ js/myscript.js
@@ -3,4 +3,18 @@ const eleGrid = document.querySelector(".grid");
 for (let i =0; i <= 100; i++) {
     console.log(i)
     eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+    
+    if (i % 15 ==0) {
+        document.write("FizzBuzz" + " ");
+    }
+    else if (i % 3 == 0) {
+        document.write("Fizz" + " ");
+    }
+    else if (i % 5 == 0) {
+        document.write("Buzz" + " ");
+    }
+    else {
+        document.write(i + " ");
+    }
 }
+
