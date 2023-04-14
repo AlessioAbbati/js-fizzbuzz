b81221c97e35cb2b6727babf57bbe5f2da992a3c
AlessioAbbati <alex_117@live.it>
2023-04-14 17:30:09 +0200

    add colo in css to fizz buzz and fizzbuzz

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,18 @@
 const eleGrid = document.querySelector(".grid");
+const FB = document.querySelector(".FB") 
+const F = document.querySelector(".F")
+const B = document.querySelector(".B")
 
 for (let i =1; i <= 100; i++) {
     
     if (i % 15 == 0) {
-        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">FizzBuzz</div>`
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell FB">FizzBuzz</div>`
         console.log("FizzBuzz")
     } else if (i % 3 == 0) {
-        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">Fizz</div>`
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell F">Fizz</div>`
         console.log("Fizz")
     } else if (i % 5 == 0) {
-        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">Buzz</div>`
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell B">Buzz</div>`
         console.log("Buzz")
     } else {
         eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`
